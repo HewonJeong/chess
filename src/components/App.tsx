2d1bb39bfba8a241be28d9086c86a8dc6576c557
Hewon Jeong <hewon.jng@gmail.com>
2018-10-27 18:30:10 +0900

    design: update App style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,9 +4,14 @@ import Board from './Board';
 class App extends React.Component {
   render() {
     return (
-      <Board knightPosition={[0, 0]} />
+      <div style={style.App}>
+        <Board knightPosition={[0, 0]} />
+      </div>
     );
   }
 }
 
+const style = {
+  App: { width: 500, height: 500}
+}
 export default App;
