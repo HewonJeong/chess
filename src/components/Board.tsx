3925cc467bcc90333b72d1d7a01e8519a2e3ec6f
Hewon Jeong <hewon.jng@gmail.com>
2018-10-27 18:33:50 +0900

    feat: add renderSquare to draw full squares on board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,19 +1,49 @@
 import * as React from 'react';
 import Square from './Square';
 import Knight from './Knight';
+import { Style } from 'src/types';
 
 interface Props {
   knightPosition: number[]
 }
 
 export default class Board extends React.Component<Props> {
-  render() {
+  renderSquare(i: number) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    const black = (x + y) % 2 === 1;
+    const [knightX, knightY] = this.props.knightPosition;
+    const piece = (x === knightX && y === knightY) ? <Knight /> : null;
+
     return (
-      <div>
-        <Square black={true}>
-          <Knight />
+      <div key={i} style={style.SquareWrapper}>
+        <Square black={black}>
+          {piece}
         </Square>
       </div>
     );
   }
+
+  render() {
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
+    }
+
+    return (
+      <div style={style.Board}>
+        {squares}
+      </div>
+    );
+  }
 }
+
+const style: Style = {
+  Board: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  SquareWrapper: { width: '12.5%', height: '12.5%' }
+} 
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+// TODO: Add function type returning React.CSSProperties
+export interface Style {
+  [key: string]: React.CSSProperties
+}
