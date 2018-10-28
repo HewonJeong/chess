cd59aeda64a7bf48e68875deaec8b8a3ace6f4aa
Hewon Jeong <hewon.jng@gmail.com>
2018-10-28 22:01:50 +0900

    feat: add canDrop with overlay

--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import Square from './Square';
 import { DropTargetMonitor, ConnectDropTarget, DropTargetSpec, DropTargetConnector, DropTarget } from 'react-dnd';
-import { moveKnight } from 'src/Game';
+import { moveKnight, canMoveKnight } from 'src/Game';
 import { Piece } from 'src/constants';
-import { Style } from 'src/types';
 
 interface Props {
   x: number;
   y: number;
   connectDropTarget?: ConnectDropTarget;
   isOver?: boolean;
+  canDrop?: boolean;
 }
 
 const squareTarget: DropTargetSpec<Props> = {
+  canDrop(props) {
+    return canMoveKnight(props.x, props.y);
+  },
+  
   drop(props: Props, monitor: DropTargetMonitor) {
     moveKnight(props.x, props.y);
   }
@@ -21,13 +25,17 @@ const squareTarget: DropTargetSpec<Props> = {
 function collect(connect: DropTargetConnector, monitor: DropTargetMonitor) {
   return {
     connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
   };
 }
 
 class BoardSquare extends React.Component<Props> {
+
+  renderOverlay = (color: string) => <div style={style.overlay(color)} />
+
   render() {
-    const { x, y, connectDropTarget, isOver } = this.props;
+    const { x, y, connectDropTarget, isOver, canDrop } = this.props;
     const black = (x + y) % 2 === 1;
 
     return connectDropTarget ? connectDropTarget(
@@ -35,19 +43,25 @@ class BoardSquare extends React.Component<Props> {
         <Square black={black}>
           {this.props.children}
         </Square>
-        {isOver && <div style={style.overed} />
-        }
+        {isOver && !canDrop && this.renderOverlay('red')}
+        {!isOver && canDrop && this.renderOverlay('yellow')}
+        {isOver && canDrop && this.renderOverlay('green')}
       </div>
     ) : null;
   }
 }
-const style: Style = {
+
+interface IStyle {
+  BoardSquare: React.CSSProperties,
+  overlay: (color: string) => React.CSSProperties 
+}
+const style: IStyle = {
   BoardSquare: {
     position: 'relative',
     width: '100%',
     height: '100%'
   },
-  overed: {
+  overlay: color => ({
     position: 'absolute',
     top: 0,
     left: 0,
@@ -55,7 +69,7 @@ const style: Style = {
     width: '100%',
     zIndex: 1,
     opacity: 0.5,
-    backgroundColor: 'yellow',
-  }
+    backgroundColor: color,
+  })
 }
 export default DropTarget(Piece.Knight, squareTarget, collect)(BoardSquare);
